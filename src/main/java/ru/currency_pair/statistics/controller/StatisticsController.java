9f9cf6f4110953e452f17ff201aeaf85023e35d4
Ailys <ai.reilinn@gmail.com>
2022-08-15 22:34:28 +0300

    add log and throw exeption

--- src/main/java/ru/currency_pair/statistics/controller/StatisticsController.java
+++ src/main/java/ru/currency_pair/statistics/controller/StatisticsController.java
@@ -1,18 +1,24 @@
 package ru.currency_pair.statistics.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.currency_pair.statistics.StatisticsApplication;
 import ru.currency_pair.statistics.model.EnumOHLC;
 import ru.currency_pair.statistics.service.StatisticsService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "api/v1/statistics")
 public class StatisticsController {
+
+    static final Logger log = LoggerFactory.getLogger(StatisticsApplication.class);
 
     @Autowired
     StatisticsService service;
@@ -23,11 +29,29 @@ public class StatisticsController {
     }
 
     @GetMapping(value = "/findBy")
-    public Object getOHCLbyNameAndTimeframe(@RequestParam String name, @RequestParam(required = false) Integer timeframe){
-        if (timeframe == null) {
-            return service.getStatisticsByName().get(name);
+    public Map<Integer, Map<String, Map<EnumOHLC, Double>>> getOHCLbyNameAndTimeframe(@RequestParam String name,
+                                                                                      @RequestParam(required = false) Integer timeframe){
+        try{
+            if (timeframe == null) {
+                log.debug("Method 'getOHLCbyNameAndTimeframe' was called with name = " + name);
+                return service.getStatisticsByName().get(name);
+            }
+            else {
+                Map<String, Map<EnumOHLC, Double>> mapByTimeframe = service.getStatisticsByName().get(name).get(timeframe);
+                if (mapByTimeframe == null) {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Timeframe Not Found");
+                }
+                else{
+                    log.debug("Method 'getOHLCbyNameAndTimeframe' was called with name = " + name + ", timeframe = " + timeframe);
+                    Map<Integer, Map<String, Map<EnumOHLC, Double>>> mapByName = new HashMap<>();
+                    mapByName.put(timeframe, mapByTimeframe);
+                    return mapByName;
+                }
+            }
+        } catch (NullPointerException exception){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Name Not Found");
         }
-        else return service.getStatisticsByName().get(name).get(timeframe);
+
     }
 
 
--- src/main/java/ru/currency_pair/statistics/service/StatisticsService.java
+++ src/main/java/ru/currency_pair/statistics/service/StatisticsService.java
@@ -1,6 +1,8 @@
 package ru.currency_pair.statistics.service;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -8,6 +10,8 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import ru.currency_pair.statistics.StatisticsApplication;
 import ru.currency_pair.statistics.config.StatisticsConfig;
 import ru.currency_pair.statistics.dto.HistoryDTO;
 import ru.currency_pair.statistics.model.EnumOHLC;
@@ -21,6 +25,8 @@ import java.util.*;
 @Getter
 public class StatisticsService {
 
+    static final Logger log = LoggerFactory.getLogger(StatisticsApplication.class);
+
     private static final Duration REQUEST_TIMEOUT = Duration.ofSeconds(3);
     WebClient client = WebClient.create("http://localhost:8080/api/v1");
 
@@ -30,14 +36,13 @@ public class StatisticsService {
     Map<String, Map<Integer, Map<String, Map<EnumOHLC, Double>>>> statisticsByName = new HashMap<>();
 
     @EventListener(ApplicationReadyEvent.class)
-    private void startOHLC(){
+    public void startOHLC(){
 
         String startTime =  history(statisticsConfig.getCurrencyPairs().get(0),
                 LocalTime.MIN.format(DateTimeFormatter.ISO_LOCAL_TIME),
-                LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)).get(0).getTime();//
-        LocalTime startLocalTime1 = LocalTime.parse(startTime);
-        startLocalTime1 = startLocalTime1.minusMinutes(startLocalTime1.getMinute()).minusSeconds(startLocalTime1.getSecond());
-        //System.out.println(startLocalTime);
+                LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)).get(0).getTime();
+        LocalTime startLocalTime_ = LocalTime.parse(startTime);
+        startLocalTime_ = startLocalTime_.minusMinutes(startLocalTime_.getMinute()).minusSeconds(startLocalTime_.getSecond());
         LocalTime endLocalTime = LocalTime.now();
 
         for (String name: statisticsConfig.getCurrencyPairs()) {
@@ -46,32 +51,43 @@ public class StatisticsService {
 
             for (int timeframe: statisticsConfig.getTimeframe()){
 
-                Map<String, Map<EnumOHLC, Double>> mapOfTimeAndOHLC = addMap(endLocalTime, startLocalTime1, name, timeframe);
+                Map<String, Map<EnumOHLC, Double>> mapOfTimeAndOHLC = addMap(endLocalTime, startLocalTime_, name, timeframe);
                 statisticsByTimeframe.put(timeframe, mapOfTimeAndOHLC);
 
             }
             statisticsByName.put(name, statisticsByTimeframe);
         }
+        log.debug("OHLC Statistics was created");
+
+        // Я хотела отловить WebClientRequestException, как в шедулере и завершить приложение, но не разобралась как System.exit() работает для SpringApplication
+
     }
 
-    @Scheduled(initialDelayString = "PT030S", fixedDelayString = "PT05M")
+    @Scheduled(initialDelayString = "PT01M", fixedDelayString = "PT01M")
     private void addOHLC(){
 
-        LocalTime endTime = LocalTime.now();
-        endTime = endTime.minusSeconds(endTime.getSecond()).minusNanos(endTime.getNano());
-        LocalTime startTime = endTime.minusMinutes(5);
-        startTime = startTime.minusMinutes(startTime.getMinute());
+        try {
+            LocalTime endTime = LocalTime.now();
+            endTime = endTime.minusSeconds(endTime.getSecond()).minusNanos(endTime.getNano());
+            LocalTime startTime = endTime.minusMinutes(1);
+            startTime = startTime.minusMinutes(startTime.getMinute());
 
-        for (String name: statisticsConfig.getCurrencyPairs()){
-            Map<Integer, Map<String, Map<EnumOHLC, Double>>> statisticsByTimeframe = statisticsByName.get(name);
-            for (int timeframe: statisticsConfig.getTimeframe()){
+            for (String name: statisticsConfig.getCurrencyPairs()){
+                Map<Integer, Map<String, Map<EnumOHLC, Double>>> statisticsByTimeframe = statisticsByName.get(name);
+                for (int timeframe: statisticsConfig.getTimeframe()){
 
-                Map<String, Map<EnumOHLC, Double>> mapOfTimeAndOHLC = statisticsByTimeframe.get(timeframe);
-                Map<String, Map<EnumOHLC, Double>> addMapOfTimeAndOHLC = addMap(endTime, startTime, name, timeframe);;
-                mapOfTimeAndOHLC.putAll(addMapOfTimeAndOHLC);
+                    Map<String, Map<EnumOHLC, Double>> mapOfTimeAndOHLC = statisticsByTimeframe.get(timeframe);
+                    Map<String, Map<EnumOHLC, Double>> addMapOfTimeAndOHLC = addMap(endTime, startTime, name, timeframe);;
+                    mapOfTimeAndOHLC.putAll(addMapOfTimeAndOHLC);
 
+                }
             }
+            log.debug("OHLC Statistics was updated");
+        } catch (WebClientRequestException exception) {
+            log.warn("History service is not running");
         }
+
+
     }
 
     private Map<String, Map<EnumOHLC, Double>> addMap(LocalTime endTime, LocalTime startTime, String name, int timeframe) {
